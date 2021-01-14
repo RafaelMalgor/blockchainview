88628f886a9cac45f2035b52ff54f26dc2efbe83
Rafael Malgor <rafaelmalgor@gmail.com>
2021-01-14 20:39:46 -0300

    Title was added. Typo was corrected

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -5,7 +5,7 @@ function Faq() {
             <li>What is this?: It is a visual representation of the basic concepts behind a Proof of Work blockchain.</li>
             <li>What can I do with it?: You can play around with it. <ul>
             <li>You can add a new block and type in a payload.</li>
-            <li>You can add mine the block to get it validated.</li>
+            <li>You can mine the block to get it validated.</li>
             <li>You can tamper with the payload and see how blocks become invalid.</li>
             <li>You can increase the difficulty and see how it takes longer to mine (if you go too crazy the browser tab might crash).</li>
             </ul></li>
